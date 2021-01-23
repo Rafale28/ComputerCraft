5ec7132d8a062c5ff8ef71554dc179fb0135d6d8
Rafale <rafale1128@gmail.com>
2021-01-23 22:23:52 +0900

    test

--- programs/User/Rafale/Test/Test.lua
+++ programs/User/Rafale/Test/Test.lua
@@ -1,8 +1,10 @@
 function checkIthem(slotVal) 
     x = turtle.getItemDetail(slotVal)
-    for k, v in pairs(x) do
-        print(k,v)
-    end
+    print("name:"..x[1])
+    print("count:"..x[3])
+    --for k, v in pairs(x) do
+    --    print(k,v)
+    --end
 end
 print("Hello World!!")
 for i=1, 16 do
