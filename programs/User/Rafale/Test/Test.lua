f3c610822f63042d6e324a08d369b67a2730c1f5
Rafale <rafale1128@gmail.com>
2021-01-24 03:48:26 +0900

    test

--- programs/User/Rafale/Test/Test.lua
+++ programs/User/Rafale/Test/Test.lua
@@ -1,10 +1,11 @@
 print("Hello World!!")
 h = fs.open("time.txt", "r")
-num = h.readline()
-if num == nil then
+if h then
+    num = h.readline()
+    h.close()
+else
     num = 0
 end
-h.close()
 print("start N="..num)
 while true do
     h = fs.open("time.txt", "w")
