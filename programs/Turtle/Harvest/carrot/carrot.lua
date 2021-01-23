67158343f088d983cbd8693fd5d0fa721e6e35ae
Rafale <rafale1128@gmail.com>
2021-01-23 19:49:58 +0900

    人参収穫プログラムcommit

--- programs/Turtle/Harvest/carrot/carrot.lua
+++ programs/Turtle/Harvest/carrot/carrot.lua
@@ -5,28 +5,16 @@
 -- #############
 -- config
 CARROT_SLOT  = 1
-FUEL_SLOT   = 13
+FUEL_SLOT   = 16
 
-function chargeFuelFromChest()
-    for i = 1, 2 do
-        turtle.select(FUEL_SLOT)
-        res = turtle.compareTo(FUEL_SLOT + i)
-        if res == false then
-            turtle.select(FUEL_SLOT + i)
-            turtle.suckUp()
-        end
-    end
-end
-
-function fuelCheck(x)
-    fuelSlot1 = FUEL_SLOT + 1 
-    fuelSlot2 = FUEL_SLOT + 2 
+function fuelCheck(lane)
+    fmin = 64 * 4 * lane 
+    fmin = fmin + 10
     fuel = turtle.getFuelLevel()
     print("fuelLevel"..fuel)
-    if fuel < x then
-        turtle.select(fuelSlot1)
-        turtle.refuel(64)
-        turtle.select(fuelSlot2)
+    while fuel < fmin do
+        turtle.select(FUEL_SLOT)
+        turtle.suckUp()
         turtle.refuel(64)
         fuel = turtle.getFuelLevel()
         print("Refuel"..fuel)
@@ -64,7 +52,7 @@ function harvestrightShift()
     turtle.turnRight()
 end
 
-function toStoreCarrot()
+function toStoreCarrot(lane)
     for i = 1, 16 do
         turtle.select(CARROT_SLOT)
         if i ~= CARROT_SLOT then
@@ -76,8 +64,10 @@ function toStoreCarrot()
         end
     end
     turtle.select(CARROT_SLOT + 1)
-    turtle.suck()
-    turtle.suck()
+    for i=1, lane do
+        turtle.suck()
+        turtle.suck()
+    end
 end
 
 function plantCarrot()
@@ -126,48 +116,73 @@ function plowtrightShift()
     turtle.turnLeft()
 end
 
+function switch2LeftLane()
+    turtle.turnRight()
+    turtle.dig() 
+    res =  turtle.forward()
+    turtle.dig() 
+    res =  turtle.forward()
+    turtle.turnRight()
+end
 
-function routine()
--- #############
--- fuelcheck
-fuelCheck(250)
+function switch2RightLane()
+    turtle.turnLeft()
+    res =  turtle.back()
+    plowAndPlant()
+    res =  turtle.back()
+    turtle.turnLeft()
+end
 
--- #############
--- harvest
-harvestLine()
-harvestleftShift()
+function routine(lane)
+    -- #############
+    -- fuelcheck
+    fuelCheck(lane)
 
-harvestLine()
-harvestrightShift()
+    -- #############
+    -- harvest
+    for i=1, lane do
+        harvestLine()
+        harvestleftShift()
 
-harvestLine()
-harvestleftShift()
+        harvestLine()
+        harvestrightShift()
 
-harvestLine()
+        harvestLine()
+        harvestleftShift()
 
-toStoreCarrot()
--- #############
--- plow and plant
-plowLine()
-plowleftShift()
+        harvestLine()
+        if i < lane then
+            switch2LeftLane()
+        end
+    end
+    toStoreCarrot(lane)
+    -- #############
+    -- plow and plant
+    for i=1, lane do
+        plowLine()
+        plowleftShift()
 
-plowLine()
-plowtrightShift()
+        plowLine()
+        plowtrightShift()
 
-plowLine()
-plowleftShift()
+        plowLine()
+        plowleftShift()
 
-plowLine()
-chargeFuelFromChest()
+        plowLine()
+        if i < lane then
+            switch2RightLane()
+        end
+    end
 end
 -- #############
 -- Main
-
+-- Laneの数を指定(1Laneは32*4のブロック)
+hLane = 2
 local minute = 60
 while true do
     for i=40, 0, -1 do
         print(i.."min...")
         sleep(minute)
     end
-    routine()
+    routine(hLane)
 end
--- programs/User/Rafale/Test.lua
+++ programs/User/Rafale/Test.lua
@@ -1,1 +1,5 @@
 print("Hello World!!")
+for i=1, 16 do
+    turtle.select(i)
+    print(textutils.serialise(turtle.getItemDetail()))
+end
