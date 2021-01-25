2d4c3c17f7d54f4bb185a96861555e0078741c29
Rafale <rafale1128@gmail.com>
2021-01-26 01:39:41 +0900

    Test追加

---
+++ programs/User/Rafale/Test/Inout.lua
@@ -0,0 +1,12 @@
+
+
+while 1 do
+    if rs.getInput("left") == true then
+        rs.setOutput("right",false)
+        sleep(0.5)
+        rs.setOutput("right",true)
+        sleep(0.5)   
+    else
+        sleep(1)
+    end
+end
