912cf5cc5043fa616424d9e3f41432cb411773da
koyamanX <ckoyama.1996@gmail.com>
2021-01-30 23:51:17 +0900

    update

--- programs/User/koyaman/slaveX.lua
+++ programs/User/koyaman/slaveX.lua
@@ -1,48 +1,43 @@
-FUEL_SLOT = 16
-FIRST_SLOT = 1
+FUEL_SLOT   = 16
 
-function getFuel(fuelSlotEnd)
-	turtle.select(FUEL_SLOT)
-	turtle.suckUp()
-	turtle.refuel(64)
+function fuelCheck(min)
     fuel = turtle.getFuelLevel()
     print("fuelLevel"..fuel)
+    while fuel < min do
+        turtle.select(FUEL_SLOT)
+        turtle.suck()
+        turtle.refuel(64)
+        fuel = turtle.getFuelLevel()
+        print("Refuel"..fuel)
+    end
 end
 
-function dig()
-	while turtle.dig() do
-		os.sleep(0.5)
-	end
-	return turtle.forward()
-end
-
-function digBlocks(len)
-	for i=0, len do
-		s = dig()
-	end
-	return s
-end
-
-function workUntilDead()
-	getFuel()
-	for i=1, 32 do
+function digBlocks(n)
+	fuelCheck(n)
+	for i=1, n do
 		turtle.select(FIRST_SLOT)
-		dig()
-		while turtle.detectUp() do
-			turtle.up()
-			dig()
+		turtle.dig()
+		os.sleep(0.5)
+		if turtle.forward() then
+			while turtle.detect() do
+				turtle.dig()
+			end
+			turtle.forward()
 		end
-		while turtle.detectDown() ~= 0 do
-			turtle.down()
-		end
-
 		if turtle.getFuelLevel() == 0 then
 			break
 		end
 	end
-	print("Dead: (No fuel)")
 end
 
+function workUntilDead()
+	digBlocks(32)
+	turtle.digUp()
+	turtle.up()
+	turtle.turnLeft()
+	turtle.turnLeft()
+	digBlocks(32)
+end
 -- #############
 -- Main
 workUntilDead()
