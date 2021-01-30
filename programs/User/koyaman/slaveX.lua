31f4a5639dfdff3e3dcfb48518e73155c6e37d80
koyamanX <ckoyama.1996@gmail.com>
2021-01-30 23:14:46 +0900

    update

--- programs/User/koyaman/slaveX.lua
+++ programs/User/koyaman/slaveX.lua
@@ -1,26 +1,28 @@
 FUEL_SLOT = 16
 FIRST_SLOT = 1
 
-function fuelCheck(lane)
-    fmin = 64 * 4 * lane 
-    fmin = fmin + 10
+function getFuel(fuelSlotEnd)
+	turtle.select(FUEL_SLOT)
+	turtle.suckUp()
+	turtle.refuel(64)
     fuel = turtle.getFuelLevel()
     print("fuelLevel"..fuel)
-    while fuel < fmin do
-        turtle.select(FUEL_SLOT)
-        turtle.suckUp()
-        turtle.refuel(64)
-        fuel = turtle.getFuelLevel()
-        print("Refuel"..fuel)
-    end
 end
 
 function workUntilDead()
+	getFuel()
 	for i=1, 10 do
-		fuelCheck(0)
 		turtle.select(FIRST_SLOT)
 		turtle.dig()
 		turtle.forward()
+
+		if turtle.getFuelLevel() = 0 then
+			break
+		end
 	end
 	print("Dead: (No fuel)")
 end
+
+-- #############
+-- Main
+workUntilDead()
