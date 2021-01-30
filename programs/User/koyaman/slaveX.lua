ada0391574dcccb9a66bf7a702167b97a0c1d407
koyamanX <ckoyama.1996@gmail.com>
2021-01-30 22:48:22 +0900

    sample program

--- programs/User/koyaman/slaveX.lua
+++ programs/User/koyaman/slaveX.lua
@@ -1,1 +1,11 @@
-print("Hello,World!");
+FUEL_SLOT = 16
+FIRST_SLOT = 1
+
+function workUntilDead()
+	while true do
+		turtle.select(FIRST_SLOT)
+		turtle.dig()
+		turtle.forward()
+	end
+	print("Dead: (No fuel)")
+end
