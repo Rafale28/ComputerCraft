4feb81735aa3b9c4626f1236dab55c2d3ed38ead
yunamine <goibutuxi@gmail.com>
2021-02-14 02:11:51 +0900

    第05話ものすごいものを作った

---
+++ programs/User/Yunamine/kamado.lua
@@ -0,0 +1,37 @@
+-- #############
+-- define functions
+
+function initialize()
+    --作業台に向いているのがホームポジション
+    while turtle.inspect()["name"] ~= "crafting_table" do
+        turtle.turnLeft()
+    end
+end
+
+function routine()
+
+   turtle.turnRight()
+   
+   --アイテムが拾えたらまだいらない
+   if turtle.suck() == nil then
+
+        turtle.turnLeft()
+        turtle.turnLeft()
+        turtle.suck()
+        turtle.turnRight()
+        turtle.turnRight()
+        turtle.drop()
+        turtle.turnLeft()
+   end
+end
+
+-- #############
+-- Main
+
+-- initialize
+initialize()
+
+while true do
+    routine()
+end
+
