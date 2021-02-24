1184425df276b90b483c1b79e59042e372a79a8a
yunamine <goibutuxi@gmail.com>
2021-02-24 22:16:33 +0900

    nontan

--- programs/User/Yunamine/craftTest.lua
+++ programs/User/Yunamine/craftTest.lua
@@ -64,19 +64,10 @@ function routine()
     rowAction(9)
 
     print("ready to craft")
+    
     --4の位置に完成品を入れる
     turtle.select(4)
-    
-    --材料がなくなるまでクラフト
-    while true do
-
-        res=turtle.craft()
-
-        if res == nil then
-            break
-        end
-
-    end
+    turtle.craft()
     
     --格納
     turtle.turnRight()
