2acbfd3a8a355b8eb1d800364fb1c5ec77592265
Rafale <rafale1128@gmail.com>
2021-02-27 22:41:17 +0900

    fix

--- programs/User/Rafale/Test/Test.lua
+++ programs/User/Rafale/Test/Test.lua
@@ -1,10 +1,22 @@
 os.loadAPI("/TurtleAPI/logging.lua")
 
 print("Hello World!!")
-for i=1, 10 do
-    print("fwd")
-    logging.forward()
+if fs.exists(logging.LOG_FILE) then
+    term.write("Return to home position..")
+    logging.makeRevFile()
+    logging.backupFile(logging.LOG_FILE)
+    logging.backupFile(logging.REV_FILE)
+    shell.run(logging.REV_FILE.."-bak")
+    print("ok")
+else -- ファイルが残っていないのは初めてこのプログラム動かすということ
+    for i=1, 20 do
+      logging.forward()
+      print("fwd")
+    end
+    logging.turnRight()
+    print("turn right")
+    for i=1, 20 do
+      logging.forward()
+      print("fwd")
+    end
 end
-
-local rev_file = logging.makeRevFile()
-shell.run(rev_file)
