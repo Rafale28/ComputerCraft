5102f6cc64f2abc3edd095db4546f8215c50c7b3
yunamine <goibutuxi@gmail.com>
2021-03-03 22:33:39 +0900

    a

--- programs/User/Yunamine/xyzTest.lua
+++ programs/User/Yunamine/xyzTest.lua
@@ -19,13 +19,13 @@ xyz=XyzApi.new()
 
 fuelCheck(150)
 
-xyz.moveTo(10,10,10)
+xyz:moveTo(10,10,10)
 sleep(10)
-xyz.moveToRelative(-5,-5,-5)
+xyz:moveToRelative(-5,-5,-5)
 sleep(10)
-xyz.moveToRelative(-6,-6,-6)
+xyz:moveToRelative(-6,-6,-6)
 sleep(10)
-xyz.moveTo(-5,-5,-5)
+xyz:moveTo(-5,-5,-5)
 sleep(10)
-xyz.moveTo(0,0,0)
+xyz:moveTo(0,0,0)
 
