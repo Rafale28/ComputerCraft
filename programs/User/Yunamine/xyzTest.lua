1f872f537d0c141939e24a0b0613b4ba92e9de58
yunamine <goibutuxi@gmail.com>
2021-03-03 22:08:44 +0900

    udk

--- programs/User/Yunamine/xyzTest.lua
+++ programs/User/Yunamine/xyzTest.lua
@@ -1,6 +1,23 @@
 require "xyzAPI"
 
+FUEL_SLOT   = 16
+
+function fuelCheck(min)
+
+    fuel = turtle.getFuelLevel()
+    print("fuelLevel"..fuel)
+    while fuel < min do
+        turtle.select(FUEL_SLOT)
+        turtle.suck(64)
+        turtle.refuel(64)
+        fuel = turtle.getFuelLevel()
+        print("Refuel now:"..fuel.." min:"..min)
+    end
+end
+
 xyz=XyzApi.new()
+
+fuelCheck(150)
 
 xyz.moveTo(10,10,10)
 sleep(10)
