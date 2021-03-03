8e7df5b69fa5d97567280eaeb5aa9a4251943b97
yunamine <goibutuxi@gmail.com>
2021-03-04 00:06:43 +0900

    split

--- programs/User/Yunamine/xyzAPI.lua
+++ programs/User/Yunamine/xyzAPI.lua
@@ -1,5 +1,19 @@
 XyzApi = {}
 
+function split(str, ts)
+    -- 引数がないときは空tableを返す
+    if ts == nil then return {} end
+  
+    local t = {} ; 
+    i=1
+    for s in string.gmatch(str, "([^"..ts.."]+)") do
+      t[i] = s
+      i = i + 1
+    end
+  
+    return t
+  end
+
 logname="xyz.log"
 
 function XyzApi.new()
