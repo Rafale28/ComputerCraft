1670bacf3bbdad1f946e6dedda764fab9be00a59
Rafale <rafale1128@gmail.com>
2021-04-12 21:09:56 +0900

    fix

--- programs/User/Rafale/drawPicture/drawPicture.lua
+++ programs/User/Rafale/drawPicture/drawPicture.lua
@@ -2,10 +2,10 @@ local mon = peripheral.wrap("back")
 mon.setTextScale(0.5)
 term.redirect(mon)
 
-function playMovie(fname, num)
+function playMovie(dir, num)
     paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
     for i=1, num do
-        fname = fname..string.format("%04d", i)..".nfp"
+        fname = dir..string.format("%04d", i)..".nfp"
         img = paintutils.loadImage(fname)
         print(fname)
         paintutils.drawImage(img, 1, 1)
