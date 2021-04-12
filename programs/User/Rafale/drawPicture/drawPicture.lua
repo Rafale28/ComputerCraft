e5da9d6e026d295f0faa364aeff201c76d8b3dd6
Rafale <rafale1128@gmail.com>
2021-04-12 20:20:19 +0900

    ahoy

--- programs/User/Rafale/drawPicture/drawPicture.lua
+++ programs/User/Rafale/drawPicture/drawPicture.lua
@@ -1,6 +1,19 @@
-local image = paintutils.loadImage("test.nfp")
+local sencho = paintutils.loadImage("sencho.nfp")
 local mon = peripheral.wrap("back")
 
 mon.setTextScale(0.5)
 term.redirect(mon)
-paintutils.drawImage(image, 1, 1)
+
+while true do
+    paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
+    paintutils.drawImage(sencho, 1, 1)
+    sleep(2)
+    paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
+    for i=1,65 do
+        fname = "marine/marine_"
+        fname = fname..string.format("%04d", i)..".nfp"
+        img = paintutils.loadImage(fname)
+        paintutils.drawImage(img, 1, 1)
+        sleep(0)
+      end
+end
