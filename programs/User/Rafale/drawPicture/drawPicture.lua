9916287b26a30e208c13339229b07f04dd0bdaa7
Rafale <rafale1128@gmail.com>
2021-04-12 21:06:03 +0900

    a

--- programs/User/Rafale/drawPicture/drawPicture.lua
+++ programs/User/Rafale/drawPicture/drawPicture.lua
@@ -2,14 +2,17 @@ local mon = peripheral.wrap("back")
 mon.setTextScale(0.5)
 term.redirect(mon)
 
-while true do
+function playMovie(fname, num)
     paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
-    for i=1,64 do
-        fname = "marine/marine_"
+    for i=1, num do
         fname = fname..string.format("%04d", i)..".nfp"
         img = paintutils.loadImage(fname)
         print(fname)
         paintutils.drawImage(img, 1, 1)
         sleep(0)
-      end
+    end
+end
+while true do
+  playMovie("marine/marine_", 64)
+  playMovie("fbk/fbk_", 203)
 end
