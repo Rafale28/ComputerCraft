1712855c4e9f6ddb2bc5b04c3694c470fa6c6a66
yunamine <goibutuxi@gmail.com>
2021-04-12 15:06:01 +0900

    v

--- programs/User/Yunamine/drawPicture/drawPicture.lua
+++ programs/User/Yunamine/drawPicture/drawPicture.lua
@@ -6,9 +6,10 @@ mon.setTextScale(0.5)
 term.redirect(mon)
 
 while true do
-
+    paintutils.drawBox(1, 1, 64, 64, colors.black)
     paintutils.drawImage(sencho, 1, 1)
     sleep(1)
+    paintutils.drawBox(1, 1, 64, 64, colors.black)
     paintutils.drawImage(fbk, 1, 1)
     sleep(1)
 end
