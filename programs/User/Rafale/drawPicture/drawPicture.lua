d54571b6d6406e6d60799c8affccfba60733d4e6
Rafale <rafale1128@gmail.com>
2021-04-12 20:41:00 +0900

    a

--- programs/User/Rafale/drawPicture/drawPicture.lua
+++ programs/User/Rafale/drawPicture/drawPicture.lua
@@ -1,14 +1,9 @@
-local sencho = paintutils.loadImage("sencho.nfp")
 local mon = peripheral.wrap("back")
-
 mon.setTextScale(0.5)
 
 while true do
     paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
-    paintutils.drawImage(sencho, 1, 1)
-    sleep(2)
-    paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
-    for i=1,65 do
+    for i=1,64 do
         fname = "marine/marine_"
         fname = fname..string.format("%04d", i)..".nfp"
         img = paintutils.loadImage(fname)
