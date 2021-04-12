410d171e251a61f5cb5cf8d3db876512becccdd8
yunamine <goibutuxi@gmail.com>
2021-04-12 15:46:33 +0900

    a

--- programs/User/Yunamine/drawPicture/drawPicture.lua
+++ programs/User/Yunamine/drawPicture/drawPicture.lua
@@ -1,5 +1,17 @@
 local sencho = paintutils.loadImage("sencho.nfp")
-local fbk = paintutils.loadImage("fbk.nfp")
+local fbk = {
+    paintutils.loadImage("fbk/1.nfp"),
+    paintutils.loadImage("fbk/2.nfp"),
+    paintutils.loadImage("fbk/3.nfp"),
+    paintutils.loadImage("fbk/4.nfp"),
+    paintutils.loadImage("fbk/5.nfp"),
+    paintutils.loadImage("fbk/6.nfp"),
+    paintutils.loadImage("fbk/7.nfp"),
+    paintutils.loadImage("fbk/8.nfp"),
+
+
+
+}
 local mon = peripheral.wrap("back")
 
 mon.setTextScale(0.5)
@@ -8,8 +20,12 @@ term.redirect(mon)
 while true do
     paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
     paintutils.drawImage(sencho, 1, 1)
-    sleep(1)
+    sleep(2)
     paintutils.drawFilledBox(1, 1, 256, 256, colors.black)
-    paintutils.drawImage(fbk, 1, 1)
-    sleep(1)
+
+    for i=1,1,8 do
+        paintutils.drawImage(fbk[i], 1, 1)
+        sleep(0.1)
+      end
+
 end
