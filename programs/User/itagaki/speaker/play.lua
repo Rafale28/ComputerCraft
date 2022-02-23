5e734b5dca5b22a1b31db1a39f53355bec05220a
genya0228 <genya0228@gmail.com>
2022-02-24 01:55:08 +0900

    fix

--- programs/User/itagaki/speaker/play.lua
+++ programs/User/itagaki/speaker/play.lua
@@ -2,7 +2,7 @@ local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
 local decoder = dfpwm.make_decoder()
-for chunk in io.lines("data/ego_rock.dfpwm", 16 * 1024) do
+for chunk in io.lines("itagaki/speaker/data/ego_rock.dfpwm", 16 * 1024) do
     local buffer = decoder(chunk)
 
     while not speaker.playAudio(buffer) do
