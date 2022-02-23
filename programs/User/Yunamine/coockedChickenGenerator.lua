bc40b55b0275c8826d886eae40c6adb0e0918d83
fbk <fbk>
2022-02-23 23:52:16 +0900

    焼き鳥製造機つくってみた

---
+++ programs/User/Yunamine/coockedChickenGenerator.lua
@@ -0,0 +1,83 @@
+-- 動き
+-- 1. チキン狩り発動までの残り時間をファイルから確認　初期は20分=1200sec
+-- 2. 1で確認した時間待機(しつつ残り時間をファイルに更新していく)
+-- 3. チキン狩り(全滅するまで)
+-- 4. 卵発射(なくなるまで？)
+-- 5. 1に戻る
+
+-- #############
+-- define functions
+logname="coockedChickenGenerator.log"
+chickenHuntingTime=10
+estimate = 0
+estimateInit=1200
+
+function updateLog(e)
+
+    if fs.exists(logname) then
+        fs.delete(logname)
+    end
+
+    log = fs.open(logname, 'w')
+    log.write(e)
+    log.close()
+
+end
+
+function initialize()
+
+    print("----initializing----")
+
+    print("Open the log file")
+    -- 1. チキン狩り発動までの残り時間をファイルから確認
+    --ある場合は残り時間を更新
+    if fs.exists(logname) then
+        log = fs.open(logname, 'r')
+        estimate=tonumber(log.readLine())
+        log.close()
+
+        --ない場合は0でつくる
+    else
+        log = fs.open(logname, 'w')
+        log.write("0")
+        estimate=0
+        log.close()
+    end
+end
+ 
+function routine()
+-- 2. 1で確認した時間待機(しつつ残り時間をファイルに更新していく)
+    for i=estimate,0,-1 do 
+        sleep(1)
+        updateLog(i)
+    end
+
+-- 3. チキン狩り(全滅するまで)
+    exist=true
+    while exist do
+        local res,x=turtle.atack()
+        exist=res
+    end
+
+-- 4. 卵発射(なくなるまで？)
+    for i = 1, 16 do
+        turtle.select(i)
+        local itm = turtle.getItemDetail(i)
+        if itm then
+            turtle.dropUp(itm["count"])
+        end
+    end
+    estimate = estimateInit
+end
+
+-- #############
+-- Main
+
+-- initialize
+initialize()
+
+while true do
+    routine()
+    sleep(1)
+end
+
