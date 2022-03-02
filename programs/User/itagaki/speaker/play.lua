cb7e8158f97b14583337ac7ab55f4e7dcc48aa0d
Rafale <rafale1128@gmail.com>
2022-03-02 23:58:27 +0900

    まふまふ

--- programs/User/itagaki/speaker/play.lua
+++ programs/User/itagaki/speaker/play.lua
@@ -1,11 +1,22 @@
 local dfpwm = require("cc.audio.dfpwm")
-local speaker = peripheral.find("speaker")
+local ossan = "speaker/data/ossan.dfpwm"
+local iris = "speaker/data/iris.dfpwm"
 
-local decoder = dfpwm.make_decoder()
-for chunk in io.lines("itagaki/speaker/data/ego_rock.dfpwm", 16 * 1024) do
-    local buffer = decoder(chunk)
 
-    while not speaker.playAudio(buffer) do
-        os.pullEvent("speaker_audio_empty")
+function play(src)
+    local speaker = peripheral.find("speaker")
+    local decoder = dfpwm.make_decoder()
+    --for chunk in io.lines("itagaki/speaker/data/ego_rock.dfpwm", 16 * 1024) do
+    for chunk in io.lines(src, 16 * 1024) do
+        local buffer = decoder(chunk)
+
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
     end
+end
+
+while true do
+    play(iris)
+    play(ossan)
 end
